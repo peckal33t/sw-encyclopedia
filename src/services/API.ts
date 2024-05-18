1bd46ae847246366106e300cebf5168f09f0dd03
Petar <pecka21@proton.me>
2024-05-18 13:43:43 +0200

    Created a 'service' and fetched the communication from the API using axios.

---
+++ src/services/API.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://swapi.thehiveresistance.com/api",
+  timeout: 10000,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+export const getData = async <T>(endpoint: string) => {
+  const res = await instance.get<T>(endpoint);
+  return res.data;
+};
+
+export const getResources = async <T>(resource: string, page: number) => {
+  return getData<T>(`/${resource}/?page=${page}`);
+};
+
+export const getResourceById = async <T>(resource: string, id: number) => {
+  return getData<T>(`/${resource}/${id}`);
+};
+
+export const searchResource = <T>(
+  resource: string,
+  query: string,
+  page: number
+) => {
+  return getData<T>(`/${resource}/?search=${query}&page=${page}`);
+};
