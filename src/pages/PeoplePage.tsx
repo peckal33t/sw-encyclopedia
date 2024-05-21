786d5e2daf8a15d34a8af1c0a0ae4632c43ce3a5
Petar <pecka21@proton.me>
2024-05-21 23:41:56 +0200

    Fetched the people from the API and rendered them to the DOM.

--- src/pages/PeoplePage.tsx
+++ src/pages/PeoplePage.tsx
@@ -2,6 +2,12 @@ import { useState, useEffect, useRef } from "react";
 import * as API from "../services/API";
 import { SW_PeopleResponse } from "../types/People.types";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import Alert from "react-bootstrap/Alert";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 const PeoplePage = () => {
   const [people, setPeople] = useState<SW_PeopleResponse | null>(null);
@@ -17,7 +23,58 @@ const PeoplePage = () => {
   const searchParamsQuery = searchParams.get("query");
   const searchParamsPage = searchParams.get("page");
 
-  return <div>PeoplePage</div>;
+  const getPeople = async (resource: string, page = 1) => {
+    setPeople(null);
+    setIsLoading(true);
+    setError(null);
+    setSearchInput("");
+
+    try {
+      const data = await API.getResources<SW_PeopleResponse>(resource, page);
+      setPeople(data);
+      console.log(data);
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError("Stop with the errors!");
+      }
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getPeople("people");
+  }, []);
+
+  return (
+    <>
+      {isLoading && <p>Loading...</p>}
+      {error && <Alert variant="warning">{error}</Alert>}
+      {!isLoading && !error && people && (
+        <Row>
+          {people.data.map((person) => (
+            <Col key={person.id} xs={12} md={6} lg={4} className="mb-3">
+              <Card className="p-3">
+                <Card.Title>{person.name}</Card.Title>
+                <Card.Text>Birth year: {person.birth_year}</Card.Text>
+                <Card.Text>In films: {person.films_count}</Card.Text>
+                <Button
+                  onClick={() => {
+                    navigate(`/people/${person.id}`);
+                  }}
+                  variant="primary"
+                  size="sm"
+                >
+                  Read more
+                </Button>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      )}
+    </>
+  );
 };
 
 export default PeoplePage;
