6f6fb95c966beaa886b753ae02335a90774b9d9b
Petar <pecka21@proton.me>
2024-05-22 22:18:43 +0200

    Created searchPeople function and created a form for search. The form doesn't work yet.

--- src/pages/PlanetsPage.tsx
+++ src/pages/PlanetsPage.tsx
@@ -41,37 +41,72 @@ const PlanetsPage = () => {
     setIsLoading(false);
   };
 
+  const searchPlanets = async (searchQuery: string, page = 1) => {
+    setPlanets(null);
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const data = await API.searchResource<SW_PlanetsResponse>(
+        "planets",
+        searchQuery,
+        page
+      );
+      setPlanets(data);
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError("Stop with the errors!");
+      }
+    }
+    setIsLoading(false);
+  };
+
   useEffect(() => {
     getPlanets("planets");
   }, []);
 
   return (
     <>
-      <>
-        {isLoading && <p>Loading...</p>}
-        {error && <Alert variant="warning">{error}</Alert>}
-        {!isLoading && !error && planets && (
-          <Row>
-            {planets.data.map((planet) => (
-              <Col key={planet.id} xs={12} md={6} lg={4} className="mb-3">
-                <Card className="p-3">
-                  <Card.Title>{planet.name}</Card.Title>
-                  <Card.Text>Appears in: {planet.films_count} films</Card.Text>
-                  <Button
-                    onClick={() => {
-                      navigate(`/planets/${planet.id}`);
-                    }}
-                    variant="primary"
-                    size="sm"
-                  >
-                    Read more
-                  </Button>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        )}
-      </>
+      {isLoading && <p>Loading...</p>}
+      {error && <Alert variant="warning">{error}</Alert>}
+      <div>
+        <Form className="mb-4">
+          <Form.Group className="mb-3" controlId="searchQuery">
+            <Form.Label>Search for planet</Form.Label>
+            <Form.Control
+              placeholder="Enter your search"
+              type="text"
+              value={searchInput}
+            />
+            <div className="d-flex justify-content-end p-2">
+              <Button disabled>Search</Button>
+            </div>
+          </Form.Group>
+        </Form>
+      </div>
+      {!isLoading && !error && planets && (
+        <Row>
+          {planets.data.map((planet) => (
+            <Col key={planet.id} xs={12} md={6} lg={4} className="mb-3">
+              <Card className="p-3">
+                <Card.Title>{planet.name}</Card.Title>
+                <Card.Text>Appears in: {planet.films_count} films</Card.Text>
+                <Button
+                  onClick={() => {
+                    navigate(`/planets/${planet.id}`);
+                  }}
+                  variant="primary"
+                  size="sm"
+                >
+                  Read more
+                </Button>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
