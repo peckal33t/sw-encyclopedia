88441c6188c4927a5b65140fd0906f3294273012
Petar <pecka21@proton.me>
2024-05-23 22:07:43 +0200

    Fetched the data for the Starships from the API and rendered the stuff out to the DOM.

--- src/pages/StarshipsPage.tsx
+++ src/pages/StarshipsPage.tsx
@@ -23,7 +23,61 @@ const StarshipsPage = () => {
   const searchParamsQuery = searchParams.get("query");
   const searchParamsPage = searchParams.get("page");
 
-  return <>Starships Page</>;
+  const getStarships = async (resource: string, page = 1) => {
+    setStarships(null);
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const data = await API.getResources<SW_StarshipsResponse>(resource, page);
+      setStarships(data);
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError("Stop with the errors!");
+      }
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getStarships("starships");
+  }, []);
+
+  return (
+    <>
+      <>
+        {isLoading && <p>Loading...</p>}
+        {error && <Alert variant="warning">{error}</Alert>}
+        {!isLoading && !error && starships && (
+          <Row>
+            {starships.data.map((starship) => (
+              <Col key={starship.id} xs={12} md={6} lg={4} className="mb-3">
+                <Card className="p-3">
+                  <Card.Title>{starship.name}</Card.Title>
+                  <Card.Text>Manufacturer: {starship.manufacturer}</Card.Text>
+                  <Card.Text>
+                    Appears in: {starship.films_count} films
+                  </Card.Text>
+                  <div className="d-flex justify-content-start">
+                    <Button
+                      onClick={() => {
+                        navigate(`/starships/${starship.id}`);
+                      }}
+                      variant="primary"
+                    >
+                      Read more
+                    </Button>
+                  </div>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        )}
+      </>
+    </>
+  );
 };
 
 export default StarshipsPage;
