249f3086f6f184bd0b44938a7776057b481b23ff
Petar <pecka21@proton.me>
2024-05-24 16:12:39 +0200

    Changed some stuff in my code, specially in my functions due to pagination and added text where it says how many results there are for Vehicles. Added aswell text where after searched for a vehicles it show for what you have searched for.

--- src/pages/VehiclesPage.tsx
+++ src/pages/VehiclesPage.tsx
@@ -23,13 +23,16 @@ const VehiclesPage = () => {
   const searchParamsQuery = searchParams.get("query");
   const searchParamsPage = searchParams.get("page");
 
-  const getVehicles = async (resource: string, page = 1) => {
+  const getVehicles = async (page = 1) => {
     setVehicles(null);
     setIsLoading(true);
     setError(null);
 
     try {
-      const data = await API.getResources<SW_VehiclesResponse>(resource, page);
+      const data = await API.getResources<SW_VehiclesResponse>(
+        "vehicles",
+        page
+      );
       setVehicles(data);
     } catch (err) {
       if (err instanceof Error) {
@@ -91,7 +94,7 @@ const VehiclesPage = () => {
       setSearchParams({
         page: newPage.toString(),
       });
-      getVehicles("vehicles", newPage);
+      getVehicles(newPage);
     }
   };
 
@@ -105,38 +108,46 @@ const VehiclesPage = () => {
       setSearchInput(searchParamsQuery);
       searchVehicles(searchParamsQuery, page);
     } else {
-      getVehicles("vehicles");
+      getVehicles(page);
     }
   }, []);
 
   return (
     <>
-      <>
-        <div>
-          <Form className="mb-4" onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="searchQuery">
-              <Form.Label>Search for vehicle</Form.Label>
-              <Form.Control
-                placeholder="Enter your search"
-                type="text"
-                value={searchInput}
-                ref={inputSearchRef}
-              />
-              <div className="d-flex justify-content-end p-2">
-                <Button
-                  onClick={handleSubmit}
-                  onSubmit={handleSubmit}
-                  disabled={searchInput.trim().length < 1}
-                >
-                  Search
-                </Button>
-              </div>
-            </Form.Group>
-          </Form>
-        </div>
-        {isLoading && <p>Loading...</p>}
-        {error && <Alert variant="warning">{error}</Alert>}
-        {!isLoading && !error && vehicles && (
+      {isLoading && <p>Loading...</p>}
+      {error && <Alert variant="warning">{error}</Alert>}
+      <div>
+        <Form className="mb-4" onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="searchQuery">
+            <Form.Label>Search for vehicle</Form.Label>
+            <Form.Control
+              onChange={(e) => setSearchInput(e.target.value)}
+              placeholder="Enter your search"
+              type="text"
+              value={searchInput}
+              ref={inputSearchRef}
+            />
+            <div className="d-flex justify-content-end p-2">
+              <Button
+                onClick={handleSubmit}
+                onSubmit={handleSubmit}
+                disabled={searchInput.trim().length < 1}
+              >
+                Search
+              </Button>
+            </div>
+          </Form.Group>
+        </Form>
+      </div>
+      {!isLoading && !error && vehicles && (
+        <>
+          {vehicles.data.length > 0 && searchParamsQuery ? (
+            <p>
+              {vehicles.total} search result for "{searchParamsQuery}"
+            </p>
+          ) : (
+            <p>{vehicles.total} results showing for Vehicles</p>
+          )}
           <Row>
             {vehicles.data.map((vehicle) => (
               <Col key={vehicle.id} xs={12} md={6} lg={4} className="mb-3">
@@ -158,8 +169,8 @@ const VehiclesPage = () => {
               </Col>
             ))}
           </Row>
-        )}
-      </>
+        </>
+      )}
     </>
   );
 };
