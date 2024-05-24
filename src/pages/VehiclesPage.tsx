7d0edc2121402d2ef3534d5d5cfdeb9038ca709e
Petar <pecka21@proton.me>
2024-05-24 16:03:15 +0200

    Added the change page function and updated it in the useEffect

--- src/pages/VehiclesPage.tsx
+++ src/pages/VehiclesPage.tsx
@@ -81,13 +81,29 @@ const VehiclesPage = () => {
     searchVehicles(trimmedSearchInput, 1);
   };
 
+  const handlePageChange = (newPage: number) => {
+    if (searchParamsQuery) {
+      setSearchParams({
+        query: searchParamsQuery,
+        page: newPage.toString(),
+      });
+    } else {
+      setSearchParams({
+        page: newPage.toString(),
+      });
+      getVehicles("vehicles", newPage);
+    }
+  };
+
   useEffect(() => {
     inputSearchRef.current?.focus();
   }, []);
 
   useEffect(() => {
+    const page = searchParamsPage ? parseInt(searchParamsPage) : 1;
     if (searchParamsQuery) {
       setSearchInput(searchParamsQuery);
+      searchVehicles(searchParamsQuery, page);
     } else {
       getVehicles("vehicles");
     }
