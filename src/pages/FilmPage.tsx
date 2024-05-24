d552e8987d0ee003c5f4b6c99ce6a7d6c1c60b6c
Petar <pecka21@proton.me>
2024-05-24 22:40:55 +0200

    Removed the slash ('/') from the films in the data because I didn't get the response for the film. Also imported some stuff from react-bootstrap just to make sure the information renders out.

--- src/pages/FilmPage.tsx
+++ src/pages/FilmPage.tsx
@@ -2,6 +2,13 @@ import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { getResourceById } from "../services/API";
 import { SW_Film } from "../types/Films.types";
+import Alert from "react-bootstrap/Alert";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { CardTitle } from "react-bootstrap";
 
 const FilmPage = () => {
   const [film, setFilm] = useState<SW_Film | null>(null);
@@ -19,7 +26,7 @@ const FilmPage = () => {
     setError(null);
 
     try {
-      const data = await getResourceById<SW_Film>("/films", id);
+      const data = await getResourceById<SW_Film>("films", id);
       setFilm(data);
     } catch (err) {
       if (err instanceof Error) {
@@ -39,7 +46,26 @@ const FilmPage = () => {
     getFilmInfo(filmId);
   }, [filmId]);
 
-  return <>FilmPage</>;
+  return (
+    <>
+      {error && <Alert variant="warning">{error}</Alert>}
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && film && (
+        <div>
+          <Row>
+            <Col key={film.id} xs={12} md={6} lg={12} className="mb-3">
+              <Card>
+                <Card.Body>
+                  <CardTitle>{film.title}</CardTitle>
+                  <Card.Text>Director: {film.director}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default FilmPage;
