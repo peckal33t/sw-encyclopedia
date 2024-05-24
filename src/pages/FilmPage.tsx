240812e70eb58aedb3560a2d1e69ac34c7a2b2ca
Petar <pecka21@proton.me>
2024-05-24 22:15:02 +0200

    Imported hooks I need aswell as a function from the API service and a film type from the types.

--- src/pages/FilmPage.tsx
+++ src/pages/FilmPage.tsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { getResourceById } from "../services/API";
+import { SW_Film } from "../types/Films.types";
+
 const FilmPage = () => {
-  return <div>FilmPage</div>;
+  const [film, setFilm] = useState<SW_Film | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  return <>FilmPage</>;
 };
 
 export default FilmPage;
