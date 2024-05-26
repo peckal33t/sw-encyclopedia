9d37c7b7b98d76d543a1badc1fa7825537a60db6
Petar <pecka21@proton.me>
2024-05-26 13:08:10 +0200

    Rendered out the information for a single person. The links are not rendered yet, cause I am not finished with rendering the information for all single pages.

--- src/pages/PersonPage.tsx
+++ src/pages/PersonPage.tsx
@@ -23,7 +23,7 @@ const PersonPage = () => {
     setError(null);
 
     try {
-      const data = await getResourceById<SW_Person>("/people", id);
+      const data = await getResourceById<SW_Person>("people", id);
       setPerson(data);
     } catch (err) {
       if (err instanceof Error) {
@@ -35,7 +35,69 @@ const PersonPage = () => {
     setIsLoading(false);
   };
 
-  return <>PersonPage</>;
+  useEffect(() => {
+    const validResourceId = Number(personId);
+    if (!validResourceId) {
+      return;
+    }
+    getPersonInfo(personId);
+  }, [personId]);
+
+  return (
+    <>
+      {error && <Alert variant="warning">{error}</Alert>}
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && person && (
+        <div>
+          <Row>
+            <Col key={person.id} xs={12} md={6} lg={12} className="mb-3">
+              <Card>
+                <Card.Body>
+                  <Card.Title>
+                    <h2 className="h4">
+                      <b>{person.name}</b>
+                    </h2>
+                  </Card.Title>
+                  <Card.Title>Attributes</Card.Title>
+                  <Card.Text>
+                    <b>Birth year:</b> {person.birth_year}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Eye color:</b> {person.eye_color}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Hair color:</b> {person.hair_color}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Height:</b> {person.height}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Mass:</b> {person.mass}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Skin color:</b> {person.skin_color}
+                  </Card.Text>
+                  <Card.Title>Links</Card.Title>
+                  <Card.Text>
+                    <b>Films</b>
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Species</b>
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Starships</b>
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Vehicles</b>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default PersonPage;
