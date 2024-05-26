2b96e33229ad1f25eb06020fdb79047ddb746b4e
Petar <pecka21@proton.me>
2024-05-26 13:17:16 +0200

    Imported the stuff I need for creating PlanetPage such as hooks and bootstrap stuff for rendering. I do not use useNavigate hook yet.

--- src/pages/PlanetPage.tsx
+++ src/pages/PlanetPage.tsx
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { getResourceById } from "../services/API";
+import { SW_Planet } from "../types/Planets.types";
+import Alert from "react-bootstrap/Alert";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+
 const PlanetPage = () => {
+  const [planet, setPlanet] = useState<SW_Planet | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  const planetId = Number(id);
+
   return <>PlanetPage</>;
 };
 
