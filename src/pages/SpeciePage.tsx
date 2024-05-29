7b95440292e975178989cc8287d62ff56b5e1821
Petar <pecka21@proton.me>
2024-05-29 12:50:20 +0200

    Fetched the data from the api for a specie and rendered out the information for a specific specie.

--- src/pages/SpeciePage.tsx
+++ src/pages/SpeciePage.tsx
@@ -18,7 +18,86 @@ const SpeciePage = () => {
 
   const specieId = Number(id);
 
-  return <>SpeciePage</>;
+  const getSpecieInfo = async (id: number) => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const data = await getResourceById<SW_Specie>("species", id);
+      setSpecie(data);
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError("Stop with the errors!");
+      }
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    const validResourceId = Number(specieId);
+    if (!validResourceId) {
+      return;
+    }
+    getSpecieInfo(specieId);
+  }, [specieId]);
+
+  return (
+    <>
+      {error && <Alert variant="warning">{error}</Alert>}
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && specie && (
+        <div>
+          <Row>
+            <Col key={specie.id} xs={12} md={6} lg={12} className="mb-3">
+              <Card>
+                <Card.Body>
+                  <Card.Title>
+                    <h2 className="h4">
+                      <b>{specie.name}</b>
+                    </h2>
+                  </Card.Title>
+                  <Card.Title>Attributes</Card.Title>
+                  <Card.Text>
+                    <b>Classification:</b> {specie.classification}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Designation:</b> {specie.designation}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Average height:</b> {specie.average_height}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Average lifespan:</b> {specie.average_lifespan}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Eye colors:</b> {specie.eye_colors}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Hair colors:</b> {specie.hair_colors}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Skin colors:</b> {specie.skin_colors}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Language:</b> {specie.language}
+                  </Card.Text>
+                  <Card.Title>Links</Card.Title>
+                  <Card.Text>
+                    <b>People</b>
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Residents</b>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default SpeciePage;
