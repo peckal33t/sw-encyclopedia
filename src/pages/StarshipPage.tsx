6eaa5fd298050073f03783f736c3a0875eb0482a
Petar <pecka21@proton.me>
2024-05-29 13:01:51 +0200

    Fetched the data from the api for a starship and rendered out the information for a specific starship.

--- src/pages/StarshipPage.tsx
+++ src/pages/StarshipPage.tsx
@@ -18,7 +18,98 @@ const StarshipPage = () => {
 
   const starshipId = Number(id);
 
-  return <>StarshipPage</>;
-};
+  const getStarshipInfo = async (id: number) => {
+    setIsLoading(true);
+    setError(null);
 
+    try {
+      const data = await getResourceById<SW_Starship>("starships", id);
+      setStarship(data);
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError("Stop with the errors!");
+      }
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    const validResourceId = Number(starshipId);
+    if (!validResourceId) {
+      return;
+    }
+    getStarshipInfo(starshipId);
+  }, [starshipId]);
+
+  return (
+    <>
+      {error && <Alert variant="warning">{error}</Alert>}
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && starship && (
+        <div>
+          <Row>
+            <Col key={starship.id} xs={12} md={6} lg={12} className="mb-3">
+              <Card>
+                <Card.Body>
+                  <Card.Title>
+                    <h2 className="h4">
+                      <b>{starship.name}</b>
+                    </h2>
+                  </Card.Title>
+                  <Card.Title>Attributes</Card.Title>
+                  <Card.Text>
+                    <b>Model:</b> {starship.model}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Starship class:</b> {starship.starship_class}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Manufacturer:</b> {starship.manufacturer}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Cost in credits:</b> {starship.cost_in_credits}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Length:</b> {starship.length}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Crew:</b> {starship.crew}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Passengers:</b> {starship.passengers}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Max atmosphering seed:</b>{" "}
+                    {starship.max_atmosphering_speed}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Hyperdrive rating:</b> {starship.hyperdrive_rating}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>MGLT:</b> {starship.MGLT}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Cargo capacity:</b> {starship.cargo_capacity}
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Consumables:</b> {starship.consumables}
+                  </Card.Text>
+                  <Card.Title>Links</Card.Title>
+                  <Card.Text>
+                    <b>Pilots</b>
+                  </Card.Text>
+                  <Card.Text>
+                    <b>Films</b>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      )}
+    </>
+  );
+};
 export default StarshipPage;
