e27e4c6b8d86bbaede860fcdc5fdb3685f271feb
Petar <pecka21@proton.me>
2024-05-30 15:28:46 +0200

    Changed the background image and added some styling to a text.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,25 @@
 const HomePage = () => {
   return (
     <>
-      <h1>Welcome to the Star Wars Encyclopedia</h1>
+      <h1 className="h2">Welcome to the Star Wars Encyclopedia</h1>
       <p>
         Whether you're a lifelong fan of the galaxy far, far away or a newcomer
         eager to explore its rich lore, you've come to the right place. This
         comprehensive guide is your portal to the vast universe of Star Wars,
-        filled with detailed information about characters, planets, species, and
-        events from across the saga. Here, you can dive deep into the stories of
-        the Jedi and Sith, the Rebellion and the Empire, and all the intricate
-        threads that weave together this epic space opera. Embark on your
-        journey through hyperspace and discover the wonders of the Star Wars
-        universe.
+        filled with detailed information about films, characters, planets,
+        species, starships, vehicles and events from across the saga. Here, you
+        can dive deep into the stories of the Jedi and Sith, the Rebellion and
+        the Empire, the Clone Wars, the Galactic Civil War, and beyond. From the
+        ancient history of the Old Republic to the rise of the First Order, and
+        even the adventures of the Mandalorians, we cover it all.
+      </p>
+      <p>
+        Discover the legendary heroes like Luke Skywalker, Princess Leia, and
+        Rey, and the iconic villains such as Darth Vader, Emperor Palpatine, and
+        Kylo Ren. Learn about the intricate politics of the Galactic Senate, the
+        mystical teachings of the Jedi Order, and the dark secrets of the Sith.
+        Explore the diverse worlds of Tatooine, Coruscant, and Dagobah, and
+        uncover the mysteries of the Force that binds the galaxy together.
       </p>
       <p>May the Force be with you!</p>
     </>
