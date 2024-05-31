26dc4ee257580a25482c85782920e80443bc4e62
Petar <pecka21@proton.me>
2024-05-31 13:12:32 +0200

    Created a NavigateButton component and added a react-bootstrap Button that navigates back one page when viewing information about something.

---
+++ src/components/NavigateButton.tsx
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+
+const NavigateButton = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Button
+      onClick={() => {
+        navigate(-1);
+      }}
+      className="custom-button-text"
+      variant="dark"
+    >
+      &lArr; Back
+    </Button>
+  );
+};
+
+export default NavigateButton;
