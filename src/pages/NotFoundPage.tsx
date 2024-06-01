98ed09d0729bf3c234f7487d28b1bcf20ae11584
Petar <pecka21@proton.me>
2024-06-01 17:57:58 +0200

    Added my Loading component to the NotFoundPage, so it first shows the Loading component and after 2 seconds it renders the content.

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,29 +1,45 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
 import Error from "../assets/images/404.gif";
+import Loading from "../components/Loading";
 
 const NotFoundPage = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <Container className="py-3">
-      <h1 className="h2">Error 404</h1>
-      <h2>The page you are reaching for could Not be Found</h2>
-
-      <div className="d-flex flex-column">
-        <Image
-          style={{ width: "37vw", height: "35vh" }}
-          className="mb-3"
-          src={Error}
-          fluid
-        />
-
-        <Link to="/">
-          <Button className="custom-button-text" variant="dark">
-            Return to Home
-          </Button>
-        </Link>
-      </div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <h1 className="h2">Error 404</h1>
+          <h2>The page you are reaching for could Not be Found</h2>
+          <div className="d-flex flex-column">
+            <Image
+              style={{ width: "37vw", height: "35vh" }}
+              className="mb-3"
+              src={Error}
+              fluid
+            />
+            <Link to="/">
+              <Button className="custom-button-text" variant="dark">
+                Return to Home
+              </Button>
+            </Link>
+          </div>
+        </>
+      )}
     </Container>
   );
 };
